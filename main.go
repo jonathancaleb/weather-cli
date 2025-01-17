7b46f23930010cb53fc6959f74dae90b4b3654c3
jonathancaleb <jonathancaleb230@gmail.com>
2025-01-17 11:21:17 +0300

    add Weather struct and sample response JSON for weather API integration

--- main.go
+++ main.go
@@ -6,6 +6,32 @@ import (
 	"net/http"
 )
 
+type Weather struct {
+	Location struct {
+		Name    string `json:"name"`
+		Country string `json:"country"`
+	} `json:"location"`
+	Current struct {
+		TempC     float64 `json:"temp_c"`
+		Condition struct {
+			Text string `json:"text"`
+		} `json:"condition"`
+	} `json:"current"`
+
+	Forecast struct {
+		Forecastday []struct {
+			Hour []struct {
+				TimeEpoch int     `json:"time_epoch"`
+				TempC     float64 `json:"temp_c"`
+				Condition struct {
+					Text string `json:"text"`
+				} `json:"condition"`
+				ChanceOfRain float64 `json:"chance_of_rain"`
+			} `json:"hour"`
+		} `json:"forecastday"`
+	} `json:"forecast"`
+}
+
 func main() {
 	res, err := http.Get("https://api.weatherapi.com/v1/current.json?key=d28a46779f2d40d581375333251701&q=Kampala&aqi=no")
 	if err != nil {
